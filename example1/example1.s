a9600babf0dfc39a1c44929a745b3e0a4cf358d8
clementyan <4a037052@stust.edu.tw>
2015-03-07 16:15:46 +0800

    example verify

--- example1/example1.s
+++ example1/example1.s
@@ -23,17 +23,17 @@
 main:
 	@ args = 0, pretend = 0, frame = 16
 	@ frame_needed = 1, uses_anonymous_args = 0
-	push	{r7, lr}
-	sub	sp, sp, #16
-	add	r7, sp, #0
-	movs	r3, #10
-	str	r3, [r7, #12]
-	movs	r3, #12
-	str	r3, [r7, #8]
-	ldr	r0, [r7, #12]
-	ldr	r1, [r7, #8]
-	bl	add
-	str	r0, [r7, #4]
+	push	{r7, lr}	@將r7與lr(link address=r14)放入堆疊區（當進入副程式時，呼叫副程式的下一個指令PC值會存入lr，先存r7再存lr，SP遞減）
+	sub	sp, sp, #16	@sp=sp-16
+	add	r7, sp, #0	@r7=sp+0
+	movs	r3, #10		@r3=10 並更新CPSR值
+	str	r3, [r7, #12]	@將r3的值存入r7的值加上12後的記憶體位址空間
+	movs	r3, #12		@r3=12 並更新CPSR值
+	str	r3, [r7, #8]	@將r3的值存入r7的值加上8後的記憶體位址空間
+	ldr	r0, [r7, #12]	@r7的值加上12後的記憶體位址所指向的值載入r0
+	ldr	r1, [r7, #8]	@r7的值加上8後的記憶體位址所指向的值載入r0
+	bl	add		@跳躍至add標記，並把下一列指令的pc值存入lr
+	str	r0, [r7, #4]	@
 	ldr	r3, [r7, #4]
 	mov	r0, r3
 	adds	r7, r7, #16
@@ -50,20 +50,20 @@ add:
 	@ args = 0, pretend = 0, frame = 8
 	@ frame_needed = 1, uses_anonymous_args = 0
 	@ link register save eliminated.
-	push	{r7}
-	sub	sp, sp, #12
-	add	r7, sp, #0
-	str	r0, [r7, #4]
+	push	{r7}		@將r7的值存入堆疊中
+	sub	sp, sp, #12	@sp=sp-12
+	add	r7, sp, #0	@r7=sp+0
+	str	r0, [r7, #4]	
 	str	r1, [r7]
 	ldr	r2, [r7, #4]
 	ldr	r3, [r7]
-	add	r3, r3, r2
+	add	r3, r3, r2	@r3=r3+r2
 	mov	r0, r3
-	adds	r7, r7, #12
+	adds	r7, r7, #12	@r7=r7+12 並更新CPSR
 	mov	sp, r7
 	@ sp needed
-	ldr	r7, [sp], #4
-	bx	lr
+	ldr	r7, [sp], #4	@先將sp內的值載入r7，再sp=sp+4
+	bx	lr		@跳躍至lr
 	.size	add, .-add
 	.ident	"GCC: (Debian 4.9.1-19) 4.9.1"
 	.section	.note.GNU-stack,"",%progbits
