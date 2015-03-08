0f6021fe01469b20cd14722cb61d785c67977301
clementyan <4a037052@stust.edu.tw>
2015-03-08 10:06:52 +0800

    multiply.s

--- example2/multiply.s
+++ example2/multiply.s
@@ -1,15 +1,15 @@
 	.text
 	.global multiply
 multiply:
-	mul R0, R1, R0
-	bx lr
-	nop
+	mul R0, R1, R0		@R0=R1*RS
+	bx lr			@pc=lr & 0xfffffffe, T=lr[0]
+	nop			@mov r8,r8
 	.type multiply, function
 	.size multipy, .-multiply
 
 	.global multiplyadd
 multiplyadd:
-	mla r0, r0, r1, r2
+	mla r0, r0, r1, r2	@r0=r0*r1+r2
 	bx lr
 	nop
 	.type multiplyadd, function
@@ -17,7 +17,7 @@ multiplyadd:
 	
 	.global multiplysub
 multiplysub:
-	mls r0, r0, r1, r2
+	mls r0, r0, r1, r2	@r0=r0*r1-r2
 	bx lr
 	nop
 	.type multiplysub, function
